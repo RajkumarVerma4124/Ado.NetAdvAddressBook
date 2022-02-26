007e7571745f886779bfdf9270896f06da92fb68
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-26 18:16:56 +0530

    [Raj] Added:- The code to establish sql connection

---
+++ Ado.NetAdvAddressBook/AdressBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.NetAdvAddressBook
+{
+    public class AdressBookRepository
+    {
+        //Give path for Database Connection
+        public static string connectionString = @"Data Source=RAJ-VERMA;Initial Catalog=PayRoll_Service;Integrated Security=True;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
